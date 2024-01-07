fd0b579e7a3d3b6316318f1e161092725d2827d2
Saanvi <81965694+unicorks@users.noreply.github.com>
2024-01-07 10:16:44 +0000

    make math_2

--- math_2.py
+++ math_2.py
@@ -13,6 +13,20 @@ def calculate(n):
 
     return nums
 
-e = calculate(100)
-for num, pattern in e.items():
-    print(num, " : ", len(pattern), " : ", pattern)
+# calculate max length of alternating pattern and the number which gave it for a range of numbers
+# so that i can try finding the relation between them
+
+def calculate_more(start, end):
+    numbers = {} # dict to store number, max alt integer, pattern
+    for i in range(start, end + 1):
+        e = calculate(i)
+        lists = e.values()
+        lists = sorted(lists, key=len, reverse=True)
+        key = list(filter(lambda x: e[x] == lists[0], e))[0]
+        numbers[i] = [key, lists[0]]
+    return numbers
+
+numbers = calculate_more(2, 100)
+for k, v in numbers.items():
+    print(k, " : ", v[0], " : ", len(v[1]), " : ", v[1])
+        
